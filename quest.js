0f8873c6c61623ec173f3fec0949671c4098b77c
Rom4n Ilyin <romchik191000@gmail.com>
2024-12-31 11:07:38 +0500

    jQuery

--- quest.js
+++ quest.js
@@ -1,10 +1,31 @@
+// jQuery
+
+const $textInput = $('#textInput'),
+    $questButton = $('#questButton'),
+    $textField = $('#textField'),
+    $clearDataButton = $('#clearDataButton');
+
+$questButton.on('click', () => {
+    $questButton.text() === 'Применить' && $textInput.val() !== '' ?
+        ($textField.text($textInput.val()), $questButton.text('Удалить')) : 
+        ($textField.text(''), $questButton.text('Применить'));
+});
+
+$clearDataButton.on('click', () => {
+    if ($textInput.val()) {
+        $textInput.val('')
+    }
+});
+
+/* JavaScript
+
 const textInput = document.getElementById('textInput'),
     questButton = document.getElementById('questButton'),
     textField = document.getElementById('textField'),
     clearDataButton = document.getElementById('clearDataButton');
 
 questButton.onclick = () => {
-    questButton.textContent = (questButton.textContent === 'Применить') && textInput.value != '' ? 
+    questButton.textContent = (questButton.textContent === 'Применить') && textInput.value !== '' ? 
         (textField.textContent = textInput.value, 'Удалить') : 
         (textField.textContent = '', 'Применить');
 };
@@ -14,3 +35,5 @@ clearDataButton.onclick = () => {
         textInput.value = '';
     }
 };
+*/
+
